a599ebc4f3b8d6ac5341f2be5df3a0c9995930a1
GINEMED\dojeda <dojeda@ginemed.es>
2020-12-16 12:18:26 +0100

    Initial commit

---
+++ back-launch/services/publicaciones.service.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+	name: "publicaciones",
+	actions: {
+		create(ctx) {
+			// TODO: creando publicacion
+		},
+		delete(ctx) {
+			// TODO: borrando publicacion
+		}
+	},
+	methods: {
+		//
+		funcionPrivada() {
+
+		}
+	},
+	created() {
+		// Aqui se crean las variable locales del servicio
+
+	},
+	started() {
+
+	},
+	stopped() {
+
+	},
+};
