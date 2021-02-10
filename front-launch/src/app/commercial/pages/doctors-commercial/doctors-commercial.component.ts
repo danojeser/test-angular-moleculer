aebfde0bcc699a617e3401d058ad066ac742747f
Daniel Ojeda <danojeser@gmail.com>
2021-02-10 13:37:55 +0100

    Edited fooder and header.

--- front-launch/src/app/commercial/pages/doctors-commercial/doctors-commercial.component.ts
+++ front-launch/src/app/commercial/pages/doctors-commercial/doctors-commercial.component.ts
@@ -7,7 +7,116 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorsCommercialComponent implements OnInit {
 
-  arrayTest = new Array(7);
+  arrayTest = new Array(8);
+
+  personal = [
+      {
+        "nombre": "Direccion",
+        "personal": [
+          {
+            "nombre": "Pascual Sanchez Martín",
+            "puesto": "Ginecolo",
+            "ciudad": "Responsable del Área Médica de Ginemed",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2020/08/pascual-sanchez-ginecologo-445x332.jpg"
+          },
+          {
+            "nombre": "Fernando Sanchez Martin",
+            "puesto": "Ginécologo",
+            "ciudad": "Responsable del Area de Reproduccion Asisitida de Ginemed",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2021/01/3_Fernando%20S%C3%A1nchez-445x319.png"
+          }
+        ]
+      },
+      {
+        "nombre": "Sistemas Informaticos",
+        "personal": [
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2020/11/daniel-ojeda-analista-programador-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2020/11/daniel-ojeda-analista-programador-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2020/11/daniel-ojeda-analista-programador-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2020/11/daniel-ojeda-analista-programador-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2020/11/daniel-ojeda-analista-programador-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2020/11/daniel-ojeda-analista-programador-445x332.jpg"
+          },
+        ]
+      },
+      {
+        "nombre": "Sistemas Informaticos",
+        "personal": [
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2021/02/Gabriel-Lopez-tecnico-sistemas-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2021/02/Gabriel-Lopez-tecnico-sistemas-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2021/02/Gabriel-Lopez-tecnico-sistemas-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2021/02/Gabriel-Lopez-tecnico-sistemas-445x332.jpg"
+          },
+          {
+            "nombre": "Daniel Ojeda",
+            "puesto": "Analista - Programador",
+            "ciudad": "Sevilla",
+            "cod": "123456798Q",
+            "img": "https://www.ginemed.es/wp-content/uploads/2021/02/Gabriel-Lopez-tecnico-sistemas-445x332.jpg"
+          },
+        ]
+      }
+  ]
 
   constructor() { }
 
